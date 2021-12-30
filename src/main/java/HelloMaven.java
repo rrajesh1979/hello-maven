da546cd2bcb0b5a82f70d9a9f92e2a77db2331f9
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2021-12-30 12:08:54 -0500

    Added .gitignore, application.properties and package-info.java in test folder

--- src/main/java/HelloMaven.java
+++ src/main/java/HelloMaven.java
@@ -1,5 +1,8 @@
+import org.apache.commons.lang3.StringUtils;
+
 public class HelloMaven {
     public static void main(String[] args) {
-        System.out.println("Hello Maven!");
+        System.out.println("Hello Maven!!!! ");
+        System.out.println(StringUtils.capitalize("hello maven"));
     }
 }
