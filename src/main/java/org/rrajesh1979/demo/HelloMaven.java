935e346c48c399496ae4521e7e5d044986ab1e5d
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2022-01-02 11:10:00 -0500

    Adding CodeCov report

--- src/main/java/org/rrajesh1979/demo/HelloMaven.java
+++ src/main/java/org/rrajesh1979/demo/HelloMaven.java
@@ -6,12 +6,12 @@ public class HelloMaven {
     static String MESSAGE = "Hello World!!!!";
 
     public static String getMessage() {
-        System.out.println("MESSAGE: " + MESSAGE);
-        System.out.println(StringUtils.capitalize(MESSAGE));
+        System.out.println("Inside getMessage()");
+//        System.out.println(StringUtils.capitalize(MESSAGE));
         return MESSAGE;
     }
 
     public static void main(String[] args) {
-        System.out.println(MESSAGE + " from main method of HelloMaven!!!!");
+        System.out.println(getMessage() + " from main method of HelloMaven!!!!");
     }
 }
